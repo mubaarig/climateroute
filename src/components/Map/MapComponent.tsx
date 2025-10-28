5dc95f1f256edb21a61aaa90dd72a8d2ed192227
Mubarig <mubarig_cisman@yahoo.com>
2025-10-28 16:10:06 +0100

    Favicon.ico added and components updated

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -6,7 +6,10 @@ import { LatLngExpression } from 'leaflet';
 
 // Fix for default markers in react-leaflet
 import L from 'leaflet';
-delete (L.Icon.Default.prototype as Record<string, unknown>)._getIconUrl;
+const iconDefaultPrototype = L.Icon.Default.prototype as unknown as {
+  _getIconUrl?: () => string;
+};
+delete iconDefaultPrototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
